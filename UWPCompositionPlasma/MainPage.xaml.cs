37bebe1baccdae444e1a432e325ccda2430c7848
Daren May <daren.may@gmail.com>
2017-12-30 13:09:24 -0700

    Built out a composition based plasma sample

--- UWPCompositionPlasma/MainPage.xaml.cs
+++ UWPCompositionPlasma/MainPage.xaml.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
@@ -22,9 +27,26 @@ namespace UWPCompositionPlasma
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private Plasma _plasma;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Loaded += OnLoaded;
+            _plasma = new Plasma(150, 150, 5);
         }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            //_plasma.RenderPalette(RootGrid);
+            //_plasma.RenderRectangleGeometry(RootGrid);
+            _plasma.RenderSolidSpriteVisual(RootGrid);
+
+
+            //Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => _plasma.AnimateRectangleGeometry());
+            Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => _plasma.AnimateSpriteVisual());
+
+        }
+
     }
 }
